29643c14693b8ab48801b58c06c50b4905684d89
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-11 09:28:20 -0230

    comments to layout my tasks

--- Assignment4/StateManagement.java
+++ Assignment4/StateManagement.java
@@ -2,4 +2,26 @@ package Assignment4;
 
 public class StateManagement {
     
+    // Inner Node class
+    private static class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        private Node<T> current;
+    }
+
+    // Add a new state (clears redo history) -- this is a newNode
+
+    // undo function
+
+    // redo function
+
+    // get current state
+
+    // create a main file to test it
 }
