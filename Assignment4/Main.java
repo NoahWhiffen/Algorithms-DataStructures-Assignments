feeebf1e5d1d3294aa974f057a1abbce98b060a6
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-12 15:45:06 -0230

    updated main file to demonstrate all methods of StateManagement thouroughly

--- Assignment4/Main.java
+++ Assignment4/Main.java
@@ -2,9 +2,11 @@ package Assignment4;
 
 public class Main {
     public static void main(String[] args) {
-        StateManagement stateManagement = new StateManagement();
-        StateManagement.LinkedList history = stateManagement.new LinkedList();
+        StateManagement<Integer> stateManagement = new StateManagement<>();
+        StateManagement<Integer>.LinkedList history = stateManagement.new LinkedList();
         
+        System.out.println("---------------------------------------");
+
         history.addState(1);
         history.addState(2);
         history.addState(3);
@@ -20,6 +22,24 @@ public class Main {
         history.redo();
         System.out.println("Current state after redo: " + history.getCurrentState());
 
-        // Attempt a redo
+        // Undo twice
+        history.undo();
+        history.undo();
+        System.out.println("Current state after two more undos: " + history.getCurrentState());
+
+        // Redo again
+        history.redo();
+        System.out.println("Current state after redo: " + history.getCurrentState()); // 3
+
+        // Add a new state to clear the redo history
+        history.addState(5);
+        System.out.println("Current state after adding a new state: " + history.getCurrentState());
+
+        // Demonstrate redo error
+        history.redo();
+
+        System.out.println("Final state: " + history.getCurrentState());
+
+        System.out.println("---------------------------------------");
     }
 }
