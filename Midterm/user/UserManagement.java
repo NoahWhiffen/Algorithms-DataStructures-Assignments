17d58270108e37372ee9765cbd3e2c6088d2ea95
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-16 21:46:07 -0230

    updated userManagement class with some required functions. Added a to do list to the readme to keep track of whats left to do

--- Midterm/user/UserManagement.java
+++ Midterm/user/UserManagement.java
@@ -1,5 +1,35 @@
 package Midterm.user;
 
 public class UserManagement {
-    
+    private User[] users;
+    private int userCount;
+
+    private boolean isUserNameUnique(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].getFullName().equalsIgnoreCase(fullName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean addUser(String firstName, String lastName) {
+        if (isUserNameUnique(firstName, lastName)) {
+            users[userCount++] = new User(firstName, lastName);
+            System.out.println("User added: " + user.getFullName);
+            return true;
+        } else {
+            System.out.println("User with name " + user.getFullName + "already exists.");
+            return false;
+        }
+    }
+
+    public void printUsers() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public User getUserByName(String firstName, String lastName) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
